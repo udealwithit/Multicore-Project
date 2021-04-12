4121d23f207a70b67fb11882eb671c1589bc5ebc
yd1568 <yd1568@nyu.eu>
2021-04-12 18:42:36 -0400

    Add example program for matrix vector multiplication

---
+++ programs/matrix_vect_mult.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Example program to multiply matrix and vector
+ * Uses openmp to parallelize the application
+*/
+int main(int argc, char* argv[]) 
+{
+
+}
