a78cc90b49bfcf072fb14c152c1417704518a34b
rugvedkaranti <rugvedkarnati@gmail.com>
2021-04-16 18:41:26 -0400

    changes

--- helpers/number_generator.py
+++ helpers/number_generator.py
@@ -2,17 +2,18 @@ import random
 import sys
 
 
-rows = sys.argv[2]
-columns = sys.argv[3]
-range = sys.argv[4]
+rows = int(sys.argv[2])
+columns = int(sys.argv[3])
+maxNum = int(sys.argv[4])
 filename = sys.argv[5]
 file = open(filename,'w')
-
-if sys.argv[1] == 1:
-    for row in rows:
-        for col in columns:
-            file.write(str(random.uniform(0,range)) + " ")
+print(sys.argv)
+if sys.argv[1] == '1':
+    for row in range(rows):
+        for col in range(columns):
+            file.write(str(random.uniform(0,maxNum)) + " ")
         file.write("\n")
 else:
-    for row in columns:
-        file.write(str(random.uniform(0,range)) + "\n")
+    for row in range(columns):
+        file.write(str(random.uniform(0,maxNum)) + "\n")
+file.close()
