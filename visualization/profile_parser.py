5aa37addae8b27d39ce2fb571b0a985ddf8b2ce5
rugvedkaranti <rugvedkarnati@gmail.com>
2021-04-19 16:20:50 -0400

    New changes to implementation

--- visualization/profile_parser.py
+++ visualization/profile_parser.py
@@ -1,15 +1,11 @@
+from collections import defaultdict
 prof_file = open("build/prof", "r")
 
-timing_info = {}
-
+timing_info = defaultdict(lambda: defaultdict(float))
 lines = prof_file.readlines()
 for line in lines:
     line = line.strip()
     arr = line.split(':')
-    if (timing_info.get(arr[0], None) is None):
-        timing_info[arr[0]] = {}
-        timing_info[arr[0]][arr[1]] = float(arr[2])
-    else:
-        timing_info[arr[0]][arr[1]] += float(arr[2])
+    timing_info[arr[0]][arr[1]] += float(arr[2])
 
 print(timing_info)
